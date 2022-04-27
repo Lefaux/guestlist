4afafa0d3baa93098d94a2ac819d7f9b790b11a9
Mathias Schreiber <mathias.schreiber@typo3.com>
2022-04-27 22:39:32 +0200

    Added Dashboard Content

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Event;
 use App\Entity\Guest;
+use App\Repository\EventRepository;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -12,12 +13,28 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractDashboardController
 {
+
+    private EventRepository $eventRepository;
+
+    public function __construct(EventRepository $eventRepository)
+    {
+        $this->eventRepository = $eventRepository;
+    }
+
     /**
      * @Route("/admin", name="admin")
      */
     public function index(): Response
     {
-        return parent::index();
+        $events = [];
+        $allEvents = $this->eventRepository->findAll();
+        foreach ($allEvents as $event) {
+            $events[] = [
+                'name' => $event->getName(),
+                'numberOfGuests' => count($event->getGuests())
+            ];
+        }
+        return $this->render('dashboard.html.twig', ['events' => $events]);
     }
 
     public function configureDashboard(): Dashboard
