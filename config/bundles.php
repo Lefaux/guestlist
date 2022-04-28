f43a677f4e6f4a26c8ea948cef32c6d6cf4f033b
Mathias Schreiber <mathias.schreiber@typo3.com>
2022-04-28 09:09:24 +0200

    Refactored towards encore and bootstrap CSS

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
