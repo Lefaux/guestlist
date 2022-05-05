e66150339f44d4732c759361a9774d9dfe51155b
Mathias Schreiber <mathias.schreiber@typo3.com>
2022-05-06 00:39:08 +0200

    [FEATURE] Add CSV Download for events

--- src/Controller/Admin/EventCrudController.php
+++ src/Controller/Admin/EventCrudController.php
@@ -3,13 +3,18 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Event;
+use App\Entity\Guest;
 use App\Form\GuestType;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class EventCrudController extends AbstractCrudController
 {
@@ -27,6 +32,17 @@ class EventCrudController extends AbstractCrudController
             ;
     }
 
+    public function configureActions(Actions $actions): Actions
+    {
+        $downloadCSV = Action::new('downloadCSV', 'Download CSV', 'fa fa-file-invoice')
+            ->linkToCrudAction('downloadCSVAction')
+            ->setCssClass('btn btn-primary')
+        ;
+        $actions
+            ->add(Crud::PAGE_INDEX, $downloadCSV)
+        ;
+        return $actions;
+    }
 
     public function configureFields(string $pageName): iterable
     {
@@ -43,4 +59,21 @@ class EventCrudController extends AbstractCrudController
         ];
     }
 
+    public function downloadCSVAction(AdminContext $context): Response
+    {
+        /** @var Event $event */
+        $event = $context->getEntity()->getInstance();
+        $csvString = '';
+        // Add Header Columns
+        $csvString .= 'Vorname,Nachname,Plus' . "\n";
+        /** @var Guest $guest */
+        foreach ($event->getGuests() as $guest) {
+            $csvString .= '"' . $guest->getFirstName() . '","' . $guest->getLastName() . '",' . $guest->getPluses() . "\n";
+        }
+        $response = new Response($csvString);
+        $response->headers->set('Content-Encoding', 'UTF-8');
+        $response->headers->set('Content-Type', 'text/csv; charset=UTF-8');
+        $response->headers->set('Content-Disposition', 'attachment; filename=' . str_replace(' ', '_', $event->getName()) . '.csv');
+        return $response;
+    }
 }
